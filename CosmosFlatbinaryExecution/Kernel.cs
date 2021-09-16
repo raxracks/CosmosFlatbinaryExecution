2ea640514be872654e6e047d36f983002df095ea
raxracks <multisniperism@gmail.com>
2021-09-15 22:18:01 -0700

    initial push

---
+++ CosmosFlatbinaryExecution/Kernel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Sys = Cosmos.System;
+using Cosmos.Core;
+using Plugs;
+
+namespace CosmosFlatbinaryExecution
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Starting...");
+
+            byte[] flatbinary = {
+                0xcd, 0x80
+            };
+
+            MemoryBlock block = new MemoryBlock(0xB00000, (uint)flatbinary.Length);
+
+            block.Write8(flatbinary);
+
+            Plugs.Jumper.Jump();
+        }
+
+        protected override void Run()
+        {
+            
+        }
+    }
+}
---
+++ Plugs/Plug.cs
@@ -0,0 +1,20 @@
+﻿using IL2CPU.API.Attribs;
+using XSharp;
+using XSharp.Assembler;
+
+namespace Plugs
+{
+    public static class Jumper
+    {
+        [PlugMethod(Assembler = typeof(CallerPlug))]
+        public static void Jump() => throw null;
+    }
+
+    public class CallerPlug : AssemblerMethod
+    {
+        public override void AssembleNew(Assembler aAssembler, object aMethodInfo)
+        {
+            XS.Jump("0xB00000");
+        }
+    }
+}
